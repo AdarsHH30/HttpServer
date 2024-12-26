e532094922fc6310ad5dbb2bd8220fcde4684de5
AdarsHH30 <your@email.com>
2024-12-26 22:00:08 +0530

    Add book.json and enhance TCP server with basic HTTP GET handling

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ server_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 #Allows the socket to reuse the address after the server is closed
 server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-server_socket.setblocking(False)
+
+# server_socket.setblocking(False)
 
 #0 to 1023 are reserved ports for system use 
 server_socket.bind((SERVER_HOST,SERVER_PORT))
@@ -25,14 +26,47 @@ print(f"Server is listening on port {SERVER_PORT}...")
 
 
 while True:
-    try:
+     
+    # try:
+    #     #Accept is the function that accepts incoming connections and returns a new socket object representing the connection and the address of the client
+    #     client_socket,client_address = server_socket.accept()
+
+    #     print(f"Connection from {client_address}")
+    #     print(f"Client socket: {client_socket}")
+
+    # except :
+    #     time.sleep(1)
+    #     print("Error connecting to client") 
+    #     continue
         #Accept is the function that accepts incoming connections and returns a new socket object representing the connection and the address of the client
-        client_socket,client_address = server_socket.accept()
+    client_socket,client_address = server_socket.accept()
 
-        print(f"Connection from {client_address}")
-        print(f"Client socket: {client_socket}")
+    #get data from the client
+    #1024 is the buffer size max amout of data that can be received at once
+    request=client_socket.recv(1024).decode()
+    print(request)
 
-    except :
-        time.sleep(1)
-        print("Error connecting to client") 
-        continue
+    #split the request into lines
+    header=request.split("\n")
+    
+    #to know the about the request
+    first_header_components=header[0].split()
+
+    http_method=first_header_components[0]
+    path=first_header_components[1]
+
+    if http_method=="GET":
+        if path=="/":
+            open_file=open("index.html")
+            content=open_file.read()
+            open_file.close()
+
+            #respose to the client
+            response=f"HTTP/1.1 200 OK\n\n{content}"
+            #encode the response to bytes
+            
+    else:
+        response="HTTP/1.1 404 NOT FOUND\n\n Allow only GET method"
+    
+    client_socket.sendall(response.encode())
+    client_socket.close()
